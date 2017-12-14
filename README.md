97d66e53659def712bee43c2934e44afbce32f4a
余金堂 <shanquan54@163.com>
2017-12-14 16:43:06 +0800

    ✨init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# burgeon
+## burgeon
 burgeon
+
+## 命令行问答式员工包安装工具
+
+### Install
+
+```shell
+npm i burgeon -g
+```
+### Usage
+
+```shell
+$ helpme
+```
+
+### example
+![](https://github.com/yujintang/burgeon/doc/helpme.mov)
---
+++ doc/npmDesc.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+    md2js: `\n\n
+    ------>
+    
+    itomix内部代码生成工具，可以生成API模版、使用API生成代码、mock数据、postman请求等
+    
+    <------
+    \n\n`,
+    kml: `\n\n
+    ------>
+    
+    itomix内部包管理器，可以用来安装、升级、发布、删除包，检查包项目使用依赖等
+    
+    <------
+    \n\n`,
+    crud: `\n\n
+    ------>
+    
+    itomix 内容使用，可以将excel表结构生成orm entity 模型
+    
+    <------
+    \n\n`,
+};
